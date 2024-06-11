bf8dbf704e3606394ea1891b4582dce074398310
Mikey <michael.williams@enspiral.com>
2024-06-12 11:06:07 +1200

    Kit generator (#2)

---
+++ plopfile.js
@@ -0,0 +1,48 @@
+
+const LABEL_RE = /^[\w ]+$/
+const TAGS_RE = /^[\w-]+(,\s*[\w-]+)*$/
+
+export default function (
+  /** @type {import('plop').NodePlopAPI} */
+  plop
+) {
+  plop.setHelper('splitCommaList', (input) => {
+    return input.split(',').map(i => i.trim())
+  })
+  plop.setGenerator('kit', {
+    description: 'Generate a new kit product',
+    prompts: [
+      {
+        type: 'input',
+        name: 'label',
+        message: 'Product label',
+        validate: (input) => {
+          return LABEL_RE.test(input)
+        }
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Product description',
+        default: ''
+      },
+      {
+        type: 'input',
+        name: 'tags',
+        message: 'Product tags',
+        default: '',
+        validate: (input) => {
+          return TAGS_RE.test(input)
+        }
+      },
+    ],
+    actions: [
+      {
+        type: 'addMany',
+        destination: './products/{{ dashCase label }}',
+        base: './templates/kit',
+        templateFiles: './templates/kit/*',
+      }
+    ]
+  })
+}
