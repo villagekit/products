c8d18b772122af9d6798a90b5f8c87c1a835315a
Michael Williams <michael.williams@enspiral.com>
2024-06-14 10:41:26 +1200

    Add Bike Rack

---
+++ products/bike-rack/bike-rack.ts
@@ -0,0 +1,127 @@
+import type { Parts } from '@villagekit/design/kit'
+
+export const parts: Parts = [
+  {
+    type: 'gridbeam:z',
+    x: -1,
+    y: 0,
+    z: [0, 20],
+  },
+  {
+    type: 'gridbeam:z',
+    x: 0,
+    y: 0,
+    z: [0, 5],
+  },
+  {
+    type: 'gridbeam:z',
+    x: 1,
+    y: 0,
+    z: [0, 20],
+  },
+
+  {
+    type: 'gridbeam:z',
+    x: -2,
+    y: 0,
+    z: [15, 45],
+  },
+  {
+    type: 'gridbeam:z',
+    x: 0,
+    y: 0,
+    z: [15, 45],
+  },
+  {
+    type: 'gridbeam:z',
+    x: 2,
+    y: 0,
+    z: [15, 45],
+  },
+  {
+    type: 'gridbeam:x',
+    x: [-1, 2],
+    y: -1,
+    z: 19,
+  },
+  {
+    type: 'gridbeam:y',
+    x: -1,
+    y: [1, -9],
+    z: 20,
+  },
+  {
+    type: 'gridbeam:y',
+    x: 1,
+    y: [1, -9],
+    z: 20,
+  },
+  {
+    type: 'gridbeam:x',
+    x: [-1, 2],
+    y: -9,
+    z: 21,
+  },
+
+  {
+    type: 'gridbeam:z',
+    x: -1,
+    y: 0,
+    z: [33, 38],
+  },
+  {
+    type: 'gridbeam:z',
+    x: 1,
+    y: 0,
+    z: [33, 38],
+  },
+  {
+    type: 'gridbeam:x',
+    x: [-1, 2],
+    y: -1,
+    z: 37,
+  },
+  {
+    type: 'gridbeam:y',
+    x: -1,
+    y: [1, -9],
+    z: 38,
+  },
+  {
+    type: 'gridbeam:y',
+    x: 1,
+    y: [1, -9],
+    z: 38,
+  },
+  {
+    type: 'gridbeam:x',
+    x: [-1, 2],
+    y: -9,
+    z: 39,
+  },
+
+  {
+    type: 'gridbeam:z',
+    x: -1,
+    y: 0,
+    z: [40, 60],
+  },
+  {
+    type: 'gridbeam:z',
+    x: 1,
+    y: 0,
+    z: [40, 60],
+  },
+  {
+    type: 'gridbeam:x',
+    x: [-1, 2],
+    y: 1,
+    z: 58,
+  },
+  {
+    type: 'gridbeam:y',
+    x: 0,
+    y: [-2, 3],
+    z: 59,
+  },
+]
