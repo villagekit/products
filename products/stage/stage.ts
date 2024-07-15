e5040098cc8c18b06b925841f150a6fe2bf3ff99
Michael Williams <hello@mikey.nz>
2024-07-15 17:01:37 +1200

    supports

--- products/stage/stage.ts
+++ products/stage/stage.ts
@@ -4,7 +4,7 @@ const numUnitsX = 2
 const numUnitsY = 2
 const unitSizeX = 30
 const unitSizeY = 20
-const unitSupports = 2
+const unitNumSupports = 2
 const height = 5
 
 export const parts: Parts = [
@@ -15,7 +15,7 @@ export const parts: Parts = [
           createStageUnit({
             sizeX: unitSizeX,
             sizeY: unitSizeY,
-            supports: unitSupports,
+            numSupports: unitNumSupports,
             height,
             offsetX: xIndex * unitSizeX,
             offsetY: yIndex * unitSizeY,
@@ -29,14 +29,14 @@ export const parts: Parts = [
 type StageUnitOptions = {
   sizeX: number
   sizeY: number
-  supports: number
+  numSupports: number
   height: number
   offsetX: number
   offsetY: number
 }
 
 function createStageUnit(options: StageUnitOptions): Parts {
-  const { sizeX, sizeY, supports, height, offsetX, offsetY } = options
+  const { sizeX, sizeY, numSupports, height, offsetX, offsetY } = options
 
   const parts = [
     // left front beam z
@@ -58,24 +58,35 @@ function createStageUnit(options: StageUnitOptions): Parts {
     // right beam y
     { type: 'gridbeam:y', x: sizeX - 1, y: [0, sizeY], z: height - 2 },
 
+    // supports
+    range(numSupports).map((supportIndex) => {
+      const supportOffsetMultiplier = 1 / (numSupports + 1)
+      const supportOffset = Math.round(supportOffsetMultiplier * sizeY * (supportIndex + 1)) - 1
+      return { type: 'gridbeam:x', x: [0, sizeX], y: supportOffset, z: height - 1 }
+    }),
+
     // top panel
     { type: 'gridpanel:xy', x: [0, sizeX], y: [0, sizeY], z: height },
   ] satisfies Parts
 
-  return parts.map((part) => {
+  return parts.map(function offsetPart(part): Parts {
+    if (Array.isArray(part)) return part.map(offsetPart)
+
     switch (part.type) {
       case 'gridbeam:x':
-        return { ...part, x: [part.x[0] + offsetX, part.x[1] + offsetX], y: part.y + offsetY }
+        return [{ ...part, x: [part.x[0] + offsetX, part.x[1] + offsetX], y: part.y + offsetY }]
       case 'gridbeam:y':
-        return { ...part, x: part.x + offsetX, y: [part.y[0] + offsetY, part.y[1] + offsetY] }
+        return [{ ...part, x: part.x + offsetX, y: [part.y[0] + offsetY, part.y[1] + offsetY] }]
       case 'gridbeam:z':
-        return { ...part, x: part.x + offsetX, y: part.y + offsetY }
+        return [{ ...part, x: part.x + offsetX, y: part.y + offsetY }]
       case 'gridpanel:xy':
-        return {
-          ...part,
-          x: [part.x[0] + offsetX, part.x[1] + offsetX],
-          y: [part.y[0] + offsetY, part.y[1] + offsetY],
-        }
+        return [
+          {
+            ...part,
+            x: [part.x[0] + offsetX, part.x[1] + offsetX],
+            y: [part.y[0] + offsetY, part.y[1] + offsetY],
+          },
+        ]
     }
   })
 }
